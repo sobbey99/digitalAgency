7c8bc3d789857cbb8e3773ed917f50820a1ca7a1
sobbey99 <sobbey99@gmail.com>
2023-02-06 22:59:52 +0200

    Quality Section

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,27 +2,26 @@ document.addEventListener('DOMContentLoaded', () => {
     //ConsultModal( from header)
     const headerBtnTriger = document.querySelector('.header-contact__modaltriger');
     const consultModal = document.querySelector('#consult-modal');
-    const consultCloseModal = document.querySelector('.dialog__close');
+    const consultCloseModal = document.querySelector('#consult-modal .dialog__close');
     headerBtnTriger.addEventListener('click', () => {
         consultModal.style.display = 'flex';
     });
     consultCloseModal.addEventListener('click', () => {
         consultModal.style.display = 'none';
-    })
+    });
+
+    //QualityModal(from sections)
+    const qualityBtnTriger = document.querySelector('.quality-speak__button');
+    const qualityModal = document.querySelector('#quality-modal');
+    const qualityCloseModal = document.querySelector('#quality-modal .dialog__close');
+    qualityBtnTriger.addEventListener('click', () => {
+        qualityModal.style.display = 'flex';
+    });
+    qualityCloseModal.addEventListener('click', () => {
+        qualityModal.style.display = 'none';
+    });
 
 
-
-    // $(window).scroll(function() {
-			
-    //     if($(window).scrollTop() > height_el) {
-        
-    //         element.addClass("fixed");
-
-    //     } else {
-        
-    //         element.removeClass("fixed");
-
-    //     }
 
     // });
     //Header Change on Scroll
