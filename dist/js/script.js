dc82d257e0aa2844278e7489391184f95f2fac5d
sobbey99 <sobbey99@gmail.com>
2023-02-06 21:14:42 +0200

    Header Fixed on Scroll

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    //ConsultModal( from header)
     const headerBtnTriger = document.querySelector('.header-contact__modaltriger');
     const consultModal = document.querySelector('#consult-modal');
     const consultCloseModal = document.querySelector('.dialog__close');
@@ -8,4 +9,31 @@ document.addEventListener('DOMContentLoaded', () => {
     consultCloseModal.addEventListener('click', () => {
         consultModal.style.display = 'none';
     })
+
+
+
+    // $(window).scroll(function() {
+			
+    //     if($(window).scrollTop() > height_el) {
+        
+    //         element.addClass("fixed");
+
+    //     } else {
+        
+    //         element.removeClass("fixed");
+
+    //     }
+
+    // });
+    //Header Change on Scroll
+    const header = document.querySelector('.header');
+    const body = document.querySelector('body');
+    window.addEventListener('scroll', checkScroll);
+    function checkScroll() {
+        if(window.scrollY >  80) {
+            header.classList.add('header_scroll')
+        } else {
+            header.classList.remove('header_scroll');
+        }
+    }
 });
